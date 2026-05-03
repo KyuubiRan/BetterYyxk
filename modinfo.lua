70418eed2c46be51819cd5ed658ba58f2cdac8f5
Kitsune <KyuubiRan@outlook.com>
2026-05-04 02:33:20 +0800

    chore: prepare 1.0.0 release assets

--- modinfo.lua
+++ modinfo.lua
@@ -1,10 +1,11 @@
-name = "空心夜雨"
-description = "夜雨空心辅助mod"
+name = "「空心夜雨」"
+description = "夜雨空心辅助模组 重置版"
 author = "KyuubiRan"
-version = "0.1.0"
+version = "1.0.0"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 
 api_version = 10
 dst_compatible = true
 client_only_mod = true
 all_clients_require_mod = false
-
